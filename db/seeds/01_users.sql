8262cf77a2a58d4062b24aafc53ead3659753439
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-02-02 10:25:46 -0500

    fix seed file, more target columns, than values

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,11 +2,11 @@
 INSERT INTO users (
   name, address, city, province, postal, credit, email, password)
 VALUES (
-  'Alice', '314 Queen St.', 'Toronto', 'ON', 'mf53ku', 'alice@qmail.com', 'password');
+  'Alice', '314 Queen St.', 'Toronto', 'ON', 'mf53ku', '999-credit', 'alice@qmail.com', 'password');
 
 
 INSERT INTO users (
   name, address, city, province, postal, credit, email, password)
 VALUES (
-  'Raymond', '65 Dufferin St.', 'Toronto', 'ON', 'g935d2', 'rayray@qmail.com', 'password'
+  'Raymond', '65 Dufferin St.', 'Toronto', 'ON', 'g935d2', '999-credit', 'rayray@qmail.com', 'password'
 );
