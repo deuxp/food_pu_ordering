3ac4ecaaa47132519f93d0c60755147d5350b5d8
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-02-01 22:41:47 -0500

    add migrations schemas

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,12 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (
+  name, address, city, province, postal, credit, email, password)
+VALUES (
+  'Alice', '314 Queen St.', 'Toronto', 'ON', 'mf53ku', 'alice@qmail.com', 'password');
+
+
+INSERT INTO users (
+  name, address, city, province, postal, credit, email, password)
+VALUES (
+  'Raymond', '65 Dufferin St.', 'Toronto', 'ON', 'g935d2', 'rayray@qmail.com', 'password'
+);
