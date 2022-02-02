c2249ac3c3ea8f66c1e883c370fd03ee28821301
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-02-02 10:32:19 -0500

    fix time format on seed

--- db/seeds/02_restaurants.sql
+++ db/seeds/02_restaurants.sql
@@ -2,5 +2,5 @@ INSERT INTO restaurants (
   name, description, address, city, province, phone, email, open, close, password
 )
 VALUES (
-   'The Rusty Napkin', 'burgers, fries, shakes for days', '414 Dupont St.', 'Toronto', 'ON', '+416555555', 'rusty@qmail.com', 1100, 1100, 'password'
+   'The Greazy Skillet', 'burgers, fries, shakes for days', '414 Dupont St.', 'Toronto', 'ON', '+416555555', 'greazy@qmail.com', '1100', '2300', 'password'
 );
