1bd350a92249ac6f1b3ae1ed9fc1b3661cf2e3bd
Alison Harman <alison_harman@sympatico.ca>
2022-02-03 15:45:39 +0000

    add custom route for client page where menu items are shown

--- server.js
+++ server.js
@@ -37,11 +37,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const itemsRoutes = require("./routes/items");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/items", itemsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -51,12 +53,6 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.get("/", (req, res) => {
   res.render("index");
 });
-
-/****temporary routing */
-app.get("/items", (req, res) => {
-  res.render("menu");
-});
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
