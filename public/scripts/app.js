9328166831dccddd16c2aca806b469f3c872f9cd
Alison Harman <alison_harman@sympatico.ca>
2022-02-03 20:37:40 +0000

    add event listener on menu button and JS to count number of items added to order

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,31 @@
 // Client facing scripts here
+$(document).ready(function() {
+
+  let orderedItems = {};
+
+  $(".menu-button").on("click", function(event) {
+    //console.log("A button has been clicked");
+    //console.log(event);
+    //console.log(this.id);
+    //console.log($(this).siblings('p').text());
+    const price = $(this).siblings('.menu-item-price').text();
+    const name = $(this).parent().parent().children('.menu-item-details-left-top').children('.menu-item-name').text();
+
+    console.log(trackOrderedItems(this.id, name, price));
+  });
+
+  const trackOrderedItems = function(buttonID, name, price) {
+    //console.log(buttonID, name, price);
+
+    if (!orderedItems[buttonID]) {
+      orderedItems[buttonID] = {id: buttonID, name: name, price: price, quantity: 1}
+    } else {
+      let qty = orderedItems[buttonID]["quantity"];
+      orderedItems[buttonID] = {id: buttonID, name: name, price: price, quantity: qty+1};
+    }
+
+    return orderedItems;
+
+  }
+
+});
