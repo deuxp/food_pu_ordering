3520a1542a1e1b9ef67efdbc0d6174d3aaeffa44
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-02-03 00:25:04 -0500

    add a test order to the seeds

---
+++ db/seeds/04_order.sql
@@ -0,0 +1,5 @@
+INSERT INTO order_items (order_id, item_id, modification, qty)
+VALUES (1, 2, 'not too hot pls', 3);
+
+INSERT INTO order_items (order_id, item_id, modification, qty)
+VALUES (1, 4, 'extra salty, pls', 1);
