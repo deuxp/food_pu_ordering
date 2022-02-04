97e46102f4b4dcd629af9e73dc1d36d0aac33b0f
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-02-04 00:44:48 -0500

    add event listener for .time-remaining-button

--- public/scripts/chefApp.js
+++ public/scripts/chefApp.js
@@ -19,26 +19,24 @@ $('document').ready(() => {
 
 
   /**
-   *
-   * @param {[{}]} items the queried list of item objects that the customer ordered
+   *  To be used inside of the billTicket -- builds the element to be appended
+   * @param {{}} items a single queried item object that the customer ordered
    */
   const billItem = function(item) {
-
-    // it will loop in the other helper function
-    // so just concern yourself with on e elem
-    // { id: 1, name: burg, modification: 'sdfsdf, qty: 0.00}
-
     // need to sanitize STILL!! ++++++++++++++++++++++++++
     const elem = `<div class="order-view">
     <div class="food-name">${item.name}</div>
     <div class="food-instructions">${item.modification}</div>
     <div class="food-qty">qty: ${item.qty}</div>
     </div>`
-
     // ghost burger
   $( ".chit" ).append(elem)
   };
 
+  /**
+   * The purposes of this function is to append a list of objects to a DOM element
+   * @param {[{}]} items
+   */
   const billTicket = items => {
     items.forEach(item => {
       billItem(item)
@@ -46,21 +44,32 @@ $('document').ready(() => {
   }
 
 
-
+  // order pending
   $('.pending-button').on('click', e => {
     e.preventDefault()
 
+    $('article').removeClass('hidden')
     // extract form data
     // need to sanitize STILL!! ++++++++++++++++++++++++++
     const order = $('#order-number').val()
-
     $.ajax({method: 'POST', data: {'id': order}, url: '/api/chefs/pending', name: 'pending'})
     .then(items => {
-      // what do I do with the items ??
+      // clear bil first
+      $('article').children().remove()
       // helper function
       billTicket(items);
     })
     .catch(err => {console.error('wha happun')})
+  })
+
+
+  // listener for time remaining button
+  // with .prev() sibling traversal on this to find the value instad of re-targeting with an id.. but if it doesn't work too well then use an id target
+  $('.time-remaining-button').on('click', function(e) {
+    e.preventDefault()
+    const $value = $('.time-remaining').val()
+
+
   })
 
 
