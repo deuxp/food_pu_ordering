e69bef73025b29247cdc9b68d699103702dbfc33
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-02-03 22:18:34 -0500

    setup endpoint to query an order, specified by number

--- routes/chefs.js
+++ routes/chefs.js
@@ -9,7 +9,7 @@ module.exports = (db) => {
     db.query(query)
       .then(data => {
         const items = data.rows;
-        console.log(items)
+        // console.log(items)
         res.render("chefs");
       })
       .catch(err => {
@@ -21,25 +21,35 @@ module.exports = (db) => {
   });
 
 
-  // POST: order number
+  // // POST: order number
   router.post('/pending', (req, res) => {
 
-    const order = req.body.order
+    const order = req.body.id
 
-    let query = `SELECT orders.id, items.name, modification, price
+    let query = `SELECT orders.id, items.name, modification, qty
     FROM order_items
     JOIN orders ON orders.id = order_id
     JOIN items ON item_id = items.id
     where orders.id = $1;`
 
     db.query(query, [order])
-      .then(data => res.send(data.rows))
-
+    .then(data => {
+        return res.send(data.rows)
+      })
     // res.send(order)
     // res.redirect('/', templateVars);
   });
 
 
+  // ready button endpoint
+  router.get('/ping', (req, res) => {
+    // this is test data, but wht it should do is set the status to paid, or closed
+    db.query('select * from items;')
+    .then(items => {
+
+      return res.send(items.rows)
+    })
+  })
 
   return router;
 };
