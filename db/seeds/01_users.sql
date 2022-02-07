56b9ce417126daa9731d57c7f82e3abec09fcc13
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-02-07 15:35:24 -0500

    add phone column to schema and seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,12 +1,13 @@
 -- Users table seeds here (Example)
 INSERT INTO users (
-  name, address, city, province, postal, credit, email, password)
+  name, address, city, province, postal, credit, email, password, phone)
 VALUES (
-  'Alice', '314 Queen St.', 'Toronto', 'ON', 'mf53ku', '999-credit', 'alice@qmail.com', 'password');
+  'Alice', '314 Queen St.', 'Toronto', 'ON', 'mf53ku', '999-credit', 'alice@qmail.com', 'password', '+19995558888'
+  );
 
 
 INSERT INTO users (
-  name, address, city, province, postal, credit, email, password)
+  name, address, city, province, postal, credit, email, password, phone)
 VALUES (
-  'Raymond', '65 Dufferin St.', 'Toronto', 'ON', 'g935d2', '999-credit', 'rayray@qmail.com', 'password'
+  'Raymond', '65 Dufferin St.', 'Toronto', 'ON', 'g935d2', '999-credit', 'rayray@qmail.com', 'password', '+19995558888'
 );
