fbad13d909dfef6433e85dc9ccb7f9f0219ecaa1
Alison Harman <alison_harman@sympatico.ca>
2022-02-08 20:48:37 +0000

    add placeholder post request handler when user submits order

--- routes/items.js
+++ routes/items.js
@@ -20,5 +20,18 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  // *** needs to be completed ***
+  // create order in database using INSERT
+  // once order is created and ID of order is returned, create rows on order_items table based on data held by cartItems
+  // then send text to restaurant
+  // some indicator of success should be sent to client page?
+  router.post('/orders', (req,res) => {
+
+    res.json(req.body);
+
+  });
+
+
   return router;
 };
