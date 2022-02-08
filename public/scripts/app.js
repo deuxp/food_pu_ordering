b4919f41068c8093258fb48cd268951466279b98
Alison Harman <alison_harman@sympatico.ca>
2022-02-08 18:46:13 +0000

    create remove button for each cart items and will remove from view and array' git status

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,57 +1,26 @@
 // Client facing scripts here
 $(document).ready(function () {
 
-  let orderedItems = [];
-
-  $(".menu-button").on("click", function (event) {
-    //const price = $(this).siblings('.menu-item-price').text();
-    const price = $(this).siblings('.menu-item-price')[0].name;
-    const name = $(this).parent().parent().children('.menu-item-details-left-top').children('.menu-item-name').text();
-
-    renderItemsToInvoice(trackOrderedItems(this.id, name, price), '#ordered-items');
-
-    renderTotals(orderedItems,'#order-totals');
-  });
+  let cartItems = [];
 
   $(".add-to-cart-button").on("click", function(event){
-    //console.log("I am clicked!")
     const quantity = ($(this).siblings('.quantity')[0].value);
     const name = $(this).parent().parent().parent().find(".menu-item-name")[0].innerText;
     const description = $(this).parent().parent().parent().find(".menu-item-description")[0].innerText;
     const price = $(this).parent().parent().parent().find(".menu-item-price")[0].name;
     const instructions = $(this).parent().parent().find("#instructions").val();
+    //  Add to Cart buttons have ID's assigned to them based on their order starting from 1. mid == "menu id"
+    const mid = this.id;
+    // create object to hold all item data
+    const item = {mid, name, description, price, instructions, quantity};
+    cartItems.push(item);
+
+    renderCart(cartItems,'#ordered-items');
+
+    renderCartTotals(cartItems, '#order-totals');
   })
 
-  const doesItemExist = function (buttonID, orderedItems) {
-    let exists = false;
-    orderedItems.forEach(item => {
-      if (item["content"].id === buttonID) {
-        exists = true;
-      }
-    })
-    return exists;
-  };
-
-  const trackOrderedItems = function(buttonID, name, price) {
-
-    const itemExists = doesItemExist(buttonID, orderedItems);
-
-    if (itemExists) {
-      orderedItems.forEach((item, index) => {
-        if (item["content"].id === buttonID) {
-          // what is the current number of items
-          const numOfItems = item["content"].quantity
-          orderedItems[index]["content"].quantity = numOfItems + 1;
-        }
-      })
-    }
-    else {
-      orderedItems.push({ content: { id: buttonID, name: name, price: price, quantity: 1 } })
-    }
-    return orderedItems;
-  };
-
-  const renderItemsToInvoice = function (items, element) {
+  const renderCart = function(items, element) {
     $(element).children().remove()
     items.forEach( (item, index) => {
       let elem =``;
@@ -60,30 +29,32 @@ $(document).ready(function () {
         <th> Name </th>
         <th> Quantity </th>
         <th> Price </th>
+        <th> Special Instuctions </th>
+        <th> Remove </th>
       </tr>`
       }
       elem += `<tr>
-      <td>${item["content"].name}</td>
-      <td>${item["content"].quantity}</td>
-      <td>$${item["content"].price/100}</td>
+      <td>${item.name}</td>
+      <td>${item.quantity}</td>
+      <td>$${item.price/100}</td>
+      <td>${item.instructions}</td>
+      <td> <button class="remove-button"> X </button> </td>
       </tr>`;
       $(element).append(elem)
     })
-  };
+  }
 
-  const renderTotals = function(items, element) {
+  const renderCartTotals = function(items, element) {
+    // clear table element
     $(element).children().remove();
-
     let total = 0;
-
+    // iterate through items to find total price in dollars
     items.forEach( item => {
-      total += item["content"].quantity*item["content"].price/100;
+      total += item.quantity*item.price/100;
     })
-
     //round total to 2 decimal places
     total = total.toFixed(2);
-
-    console.log('total', total);
+    // create HTML table element
     const elem = `<tr>
     <td>Sub-total</td>
     <td>$ ${total} </td>
@@ -98,4 +69,17 @@ $(document).ready(function () {
     `
     $(element).append(elem)
   }
+
+  $("#ordered-items").on("click", ".remove-button", function(event){
+    // rowIndex value of table row object in Cart. 1 is the first remove (X) button and so on...
+    const rowIndex = $(this).parent().parent()[0].rowIndex
+    console.log(rowIndex);
+    //mutate cartItems to remove index = rowIndex -1
+    cartItems.splice(rowIndex-1,1)
+    // need to re-do Cart View
+    renderCart(cartItems,'#ordered-items');
+
+    renderCartTotals(cartItems, '#order-totals');
+  })
+
 });
